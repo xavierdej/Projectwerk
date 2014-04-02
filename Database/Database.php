4154047ae265a8a1cb63928cb1e59059757eaf6c
Xavier <xavier@dejager.be>
2014-04-02 14:33:45 +0200

    changed Database.php to use ini file

--- Database/Database.php
+++ Database/Database.php
@@ -7,29 +7,40 @@
     {
         private static $db = NULL;
 
-        const DSN = "mysql:host=10.246.16.200;port=3306;dbname=dejager.be";
-        const USERNAME = "dejager_be@phpmyadmin01.local.one.com";
-        const PASSWORD = "";
+        const INI_FILE = "config_db.ini";
 
-        public static function getInstance() 
+        public static function getInstance()
         {
             if (self::$db) 
             {
                 return self::$db;
             }
-            try 
+            
+            // Parse config
+            if (!$settings = parse_ini_file(self::INI_FILE, TRUE))
             {
-                self::$db = new PDO(self::DSN, self::USERNAME, self::PASSWORD);
-                echo 'Connection made!!'
-                return self::$db;
+                throw new exception('Unable to open ' . self::INI_FILE);
             }
-            catch (PDOException $e) 
-            {
-                echo 'Connection failed: ' . $e->getMessage();
-                exit();
-            }        
 
+            $dsn = $settings["db_driver"] . ":";
+            foreach ($settings ["dsn"] as $key => $val)
+            {
+                $dsn .= "$key=$val;";
+            }
+        }
+
+        try 
+        {
+            self::$db = new PDO($dsn, $settings["db_user"],
+            $settings["db_password"]);
+
+            echo 'Connection successful'
             return self::$db;
+        }
+        catch (PDOException $e) 
+        {
+            echo 'Connection failed: ' . $e->getMessage();
+            exit();
         }
     }
 
